ee53390278bc58626d943658455952daf578d60f
subhakti12 <115478042+subhakti12@users.noreply.github.com>
2023-10-11 20:47:26 +0700

    Delete Praktikum1 directory

--- Praktikum1/ClassDataNilai.java
+++
@@ -1,7 +0,0 @@
-public class ClassDataNilai {
-    public static void main(String[] args) {
-        Nilai nilaiMahasiswa = new Nilai("050222", "BhaktiVia", 90, 85, 75, 80);
-        nilaiMahasiswa.CetakNilai();
-    }
-}
-
